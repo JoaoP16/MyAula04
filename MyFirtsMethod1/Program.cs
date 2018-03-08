32853173d259b2918705ca4528df5adf26e764b3
fs000 <flaviosantos002010@hotmail.com>
2018-03-08 15:50:53 -0100

    1º Commit

--- MyFirtsMethod1/Program.cs
+++ MyFirtsMethod1/Program.cs
@@ -8,8 +8,15 @@ namespace MyAula04
 {
     class MyClass
     {
+        static void Main(string[] args)
+        {
+            CountTo10();
+            CountTo10();
+        }
+
         static void CountTo10()
-        {for (int i = 1; i <= 10; i++)
+        {
+            for (int i = 1; i <= 10; i++)
                 Console.WriteLine(i);
         }
     }
